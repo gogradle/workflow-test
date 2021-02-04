17f044599ffe60a4a999888f40f76e6eaff19a35
Bo Zhang <zhangbodut@gmail.com>
2021-02-04 14:05:27 +0800

    Remove dotdotdot

--- .github/verifyWhitelist.sh
+++ .github/verifyWhitelist.sh
@@ -6,7 +6,7 @@ else
   BASE="${BASE_REV}"
 fi
 
-CHANGED_FILES=`git diff --name-only $BASE...HEAD`
+CHANGED_FILES=`git diff --name-only $BASE HEAD`
 
 CHANGED_FILE_LIST=($(echo "$CHANGED_FILES" | sed 's/"//g' | sed 's/:/ /g'))
 WHITELIST=($(cat .github/whitelist.txt | sed 's/"//g' | sed 's/:/ /g'))
